e8c32f5591edcb89466a246142f916ddb3043cd1
crisanto-dev <69776924+crisanto-dev@users.noreply.github.com>
2020-08-17 17:56:30 -0600

    Update cleanquery.php
    Usar loop es una buena practica! en este caso un "foreach" porque no se sabe el numero exacto de veces que se va a repetir

--- cleanquery.php
+++ cleanquery.php
@@ -3,39 +3,47 @@ function strClean($strCadena){
 		$string = preg_replace(['/\s+/','/^\s|\s$/'],[' '],'', $strCadena);
 		$string = trim($string); //Elimina espacios en blanco al inicio y al final
 		$string = stripcslashes($string); //Elimina las \ invertidas
-		$string = str_ireplace("<script>", "", $string);
-		$string = str_ireplace("</script>", "", $string);
-		$string = str_ireplace("<script src>", "", $string);
-		$string = str_ireplace("<script type=>", "", $string);
-		$string = str_ireplace("SELECT FROM *", "", $string);
-		$string = str_ireplace("DELETE FROM", "", $string);
-		$string = str_ireplace("UNION", "", $string);
-		$string = str_ireplace("INSERT INTO", "", $string);
-		$string = str_ireplace("SELECT COUNT(*) FROM", "", $string);
-		$string = str_ireplace("DROP TABLE", "", $string);
-		$string = str_ireplace("TRUNCATE TABLE", "", $string);
-		$string = str_ireplace("SHOW TABLES", "", $string);
-		$string = str_ireplace("SHOW DATABASES", "", $string);
-		$string = str_ireplace("OR '1'='1", "", $string);
-		$string = str_ireplace('OR "1"="1"', "", $string);
-		$string = str_ireplace('OR ´1´=´1´', "", $string);
-		$string = str_ireplace("is NULL; --", "", $string);
-		$string = str_ireplace("is NULL; --", "", $string);
-		$string = str_ireplace("LIKE'", "", $string);
-		$string = str_ireplace('LIKE "', "", $string);
-		$string = str_ireplace("LIKE ´", "", $string);
-		$string = str_ireplace("OR 'a'='a", "", $string);
-		$string = str_ireplace('OR "a"="a', "", $string);
-		$string = str_ireplace("OR ´a´=´a", "", $string);
-		$string = str_ireplace("OR ´a´=´a", "", $string);
-		$string = str_ireplace("<?php", "", $string);
-		$string = str_ireplace("?>", "", $string);
-		$string = str_ireplace("--", "", $string);
-		$string = str_ireplace("^", "", $string);
-		$string = str_ireplace("[", "", $string);
-		$string = str_ireplace("]", "", $string);
-		$string = str_ireplace("==", "", $string);
-		$string = str_ireplace(";", "", $string);
-		$string = str_ireplace("::", "", $string);
+		
+	`	$replace = [
+			"<script>",
+			"</script>",
+			"<script src>",
+			"<script type=>",
+			"SELECT FROM *",
+			"DELETE FROM",
+			"UNION",
+			"INSERT INTO",
+			"SELECT COUNT(*) FROM",
+			"DROP TABLE",
+			"TRUNCATE TABLE",
+			"SHOW TABLES",
+			"SHOW DATABASES",
+			"OR '1'='1",
+			'OR "1"="1"',
+			'OR ´1´=´1´',
+			"is NULL; --",
+			"is NULL; --",
+			"LIKE'",
+			'LIKE "',
+			"LIKE ´",
+			"OR 'a'='a",
+			'OR "a"="a',
+			"OR ´a´=´a",
+			"OR ´a´=´a",
+			"<?php",
+			"?>",
+			"--",
+			"^",
+			"[",
+			"]",
+			"==",
+			";",
+			"::"
+		];
+
+		foreach($replace as $rep)
+		{
+			$string = str_replace($rep,"",$string);
+		}
 		return $string;
 ?>
